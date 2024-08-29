c2be85f474f56616b7057677b8ce86017de49d65
yeswanth-svg <130365668+yeswanth-svg@users.noreply.github.com>
2024-08-29 13:35:28 +0530

    added some new routes and added the anime list controller file and view files for the others lits

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 
+
+
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,5 +13,10 @@ Route::get('/', [HomeController::class, 'index']);
 
 
 Route::get('/movies', [MovieListController::class, 'moviesList'])->name('movies.list');
+Route::get('/movies/wpml', [MovieListController::class, 'wpmoviesList'])->name('wpmovies.list');
+Route::get('/movies/tpml', [MovieListController::class, 'tpmoviesList'])->name('tpmovies.list');
+Route::get('/movies/trml', [MovieListController::class, 'trmoviesList'])->name('trmovies.list');
+
+
 
 
