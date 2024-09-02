9bd8dc5fa098bae46a7e1336737948d960b26fc8
yeswanth-svg <130365668+yeswanth-svg@users.noreply.github.com>
2024-09-02 13:14:51 +0530

    seperate the navbar and include it the required files

--- resources/views/lists/popluar_anime_list.blade.php
+++ resources/views/lists/popluar_anime_list.blade.php
@@ -1,74 +1,8 @@
 @extends('layouts.app')
 @section('content')
+
 <!-- Navbar Inside Each Carousel Slide -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-absolute top-0 start-0 w-100 mb-5">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-            <img src="{{ asset('images/logo3.png') }}" alt="Logo" class="img-fluid" style="max-height: 80px;">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent1"
-            aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <div class="search-box">
-                        <button class="btn-search"><i class="bi bi-search"></i></button>
-                        <input type="text" class="input-search" placeholder="Type to Search...">
-                    </div>
-
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle fs-5" href="movies" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Movies
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="movies/wpml">World Popular Movies</a></li>
-                        <li><a class="dropdown-item" href="movies/trml">Top Rated Movies</a></li>
-                        <li><a class="dropdown-item" href="movies/tpml">Telugu Popular Movies</a></li>
-
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="tv_series">Tv Series</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="anime">Anime</a>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        <i class="bi bi-person-circle"></i>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Porifle</a></li>
-                        <li><a class="dropdown-item" href="#">WatchList</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Logout</a></li>
-
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="#">Signup</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="#">Login</a>
-                </li>
-
-            </ul>
-        </div>
-    </div>
-</nav>
+@include('layouts.navbar')
 
 <div class="container col-md-12" style="margin-top:7rem !important;">
     <h1 class="mb-1 text-center text-light">Poular Anime</h1>
--- resources/views/lists/upcoming_anime_list.blade.php
+++ resources/views/lists/upcoming_anime_list.blade.php
@@ -1,74 +1,8 @@
 @extends('layouts.app')
 @section('content')
+
 <!-- Navbar Inside Each Carousel Slide -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-absolute top-0 start-0 w-100 mb-5">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-            <img src="{{ asset('images/logo3.png') }}" alt="Logo" class="img-fluid" style="max-height: 80px;">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent1"
-            aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <div class="search-box">
-                        <button class="btn-search"><i class="bi bi-search"></i></button>
-                        <input type="text" class="input-search" placeholder="Type to Search...">
-                    </div>
-
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle fs-5" href="movies" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Movies
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="movies/wpml">World Popular Movies</a></li>
-                                    <li><a class="dropdown-item" href="movies/trml">Top Rated Movies</a></li>
-                                    <li><a class="dropdown-item" href="movies/tpml">Telugu Popular Movies</a></li>
-
-                                </ul>
-                            </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="tv_series">Tv Series</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="anime">Anime</a>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        <i class="bi bi-person-circle"></i>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Porifle</a></li>
-                        <li><a class="dropdown-item" href="#">WatchList</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Logout</a></li>
-
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="#">Signup</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-light fs-5" href="#">Login</a>
-                </li>
-
-            </ul>
-        </div>
-    </div>
-</nav>
+@include('layouts.navbar')
 
 <div class="container col-md-12" style="margin-top:7rem !important;">
     <h1 class="mb-1 text-center text-light">Upcoming Anime</h1>
@@ -90,22 +24,22 @@
 
                             <h6 class="card-title">{{$title}}</h6>
                             <!-- <p><span style="color:#ffee00;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                                                fill="currentColor" class="bi bi-vignette" viewBox="0 0 16 16">
-                                                                                <path d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8" />
-                                                                                <path
-                                                                                    d="M8.5 4.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 7a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m1.683-6.281a.5.5 0 1 1-.866-.5.5.5 0 0 1 .866.5m-3.5 6.062a.5.5 0 1 1-.866-.5.5.5 0 0 1 .866.5m4.598-4.598a.5.5 0 1 1-.5-.866.5.5 0 0 1 .5.866m-6.062 3.5a.5.5 0 1 1-.5-.866.5.5 0 0 1 .5.866M11.5 8.5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m-7 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m6.281 1.683a.5.5 0 1 1 .5-.866.5.5 0 0 1-.5.866m-6.062-3.5a.5.5 0 1 1 .5-.866.5.5 0 0 1-.5.866m4.598 4.598a.5.5 0 1 1 .866-.5.5.5 0 0 1-.866.5m-3.5-6.062a.5.5 0 1 1 .866-.5.5.5 0 0 1-.866.5" />
-                                                                            </svg></span>&nbsp;{{$anime['status']}}</p>
-                                                                    <p>&nbsp;&nbsp;&nbsp;{{$anime['aired']['string']}}</p> -->
+                                                                                        fill="currentColor" class="bi bi-vignette" viewBox="0 0 16 16">
+                                                                                        <path d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8" />
+                                                                                        <path
+                                                                                            d="M8.5 4.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 7a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m1.683-6.281a.5.5 0 1 1-.866-.5.5.5 0 0 1 .866.5m-3.5 6.062a.5.5 0 1 1-.866-.5.5.5 0 0 1 .866.5m4.598-4.598a.5.5 0 1 1-.5-.866.5.5 0 0 1 .5.866m-6.062 3.5a.5.5 0 1 1-.5-.866.5.5 0 0 1 .5.866M11.5 8.5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m-7 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m6.281 1.683a.5.5 0 1 1 .5-.866.5.5 0 0 1-.5.866m-6.062-3.5a.5.5 0 1 1 .5-.866.5.5 0 0 1-.5.866m4.598 4.598a.5.5 0 1 1 .866-.5.5.5 0 0 1-.866.5m-3.5-6.062a.5.5 0 1 1 .866-.5.5.5 0 0 1-.866.5" />
+                                                                                    </svg></span>&nbsp;{{$anime['status']}}</p>
+                                                                            <p>&nbsp;&nbsp;&nbsp;{{$anime['aired']['string']}}</p> -->
 
                             <!-- <div class="star-rating">
-                                                                        @for ($i = 0; $i < 5; $i++)
-                                                                            @if ($i < $rating_out_of_five)
-                                                                                <i class="bi bi-star-fill text-warning"></i> 
-                                                                            @else
-                                                                                <i class="bi bi-star text-warning"></i> 
-                                                                            @endif
-                                                                        @endfor
-                                                                    </div> -->
+                                                                                @for ($i = 0; $i < 5; $i++)
+                                                                                    @if ($i < $rating_out_of_five)
+                                                                                        <i class="bi bi-star-fill text-warning"></i> 
+                                                                                    @else
+                                                                                        <i class="bi bi-star text-warning"></i> 
+                                                                                    @endif
+                                                                                @endfor
+                                                                            </div> -->
                         </div>
                     </div>
                 </div>
